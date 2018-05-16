d109bec82c4ff04aaca24ee8c16a4d063c56d087
le xuan trung <lexuantrung1612@gmail.com>
2018-05-17 00:51:15 +0700

    change mail

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,6 @@ class MessageMailer < ApplicationMailer
   def send_message(mail)
     @content = mail[:content]
     @email = mail[:email]
-    mail(to: "kien.arch92@gmail.com", subject: "Susurrus-message from #{mail[:name]}")
+    mail(to: ENV['OWNER_GMAIL'], subject: "Susurrus-message from #{mail[:name]}")
   end
 end
