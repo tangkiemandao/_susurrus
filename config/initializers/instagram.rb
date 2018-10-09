27020b5c972b282cdc45f5161c49b08e47a6491f
le xuan trung <lexuantrung1612@gmail.com>
2018-10-10 00:15:20 +0700

    Setting api instagram

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+Instagram.configure do |config|
+  config.client_id = "09def34fddd146d3a2f31cc288e4bbba"
+  config.client_secret = "09def34fddd146d3a2f31cc288e4bbba"
+  config.access_token = "6728794923.1677ed0.038d13fdd7b246fcb8da1e4779f41eb3"
+end
